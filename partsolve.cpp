4a7d67fb2dbf721c1840a308fb0217a2a03a8f7f
naturalog <ohadasor@gmail.com>
2014-12-13 03:39:38 +0200

    vexcl

--- partsolve.cpp
+++ partsolve.cpp
@@ -1,11 +1,13 @@
+// g++ ./partsolve.cpp  -O3 -I/opt/AMDAPP/include -std=c++11 -lOpenCL -lboost_system
 #include <iostream>
 #include <cmath>
 #include <string>
 #include <cstdlib>
-#include <Eigen/Core>
-#include <unsupported/Eigen/MPRealSupport>
+//#include <Eigen/Core>
+//#include <unsupported/Eigen/MPRealSupport>
+#include <vexcl/vexcl.hpp>
 using namespace std;
-using namespace Eigen;
+//using namespace Eigen;
 
 //#define MPREC
 
@@ -15,10 +17,10 @@ const uint prec = 1024;
 using namespace mpfr;
 typedef mpreal scalar;
 #else
-typedef long double scalar;
+typedef double scalar;
 #endif
 
-typedef Matrix<scalar, Dynamic, Dynamic> mat;
+//typedef Matrix<scalar, Dynamic, Dynamic> mat;
 const scalar one = 1;
 const scalar two = 2;
 const scalar half = one / two;
@@ -27,14 +29,17 @@ const scalar h = 1e-15;
 
 scalar *x;
 uint N;
-
-scalar psi(const scalar& t/*, uint d = 0*/) {
+/*
+scalar psi(const scalar& t, uint d = 0) {
 //	if (d) return (psi(t+h,d-1)-psi(t-h,d-1))/(h*two);
 	static const scalar pi = acos(-one);
 //	return cos(pi*(one+two)*t);
-	scalar res = 1;
-	for (uint n = 0; n < N; n++) res *= cos(t*x[n]);
-	return res;
+	scalar pos = 0, neg = 0, z;
+	for (uint n = 0; n < N; n++) 
+		( (z = cos(t * x[n])) > 0 ) ? 
+			( pos += log(z) : neg += log(-z) );
+	
+	return exp(pos-neg);
 }
 
 scalar* samples = 0;
@@ -50,19 +55,22 @@ scalar numint() {
 	const scalar prsz = pi / scalar(size);
 	for (uint k = 0; k <= size; ++k) {
 		x = scalar(k) * prsz;
-/*		y = psi(x) - c;
-		t = res + y;
-		c = (t - res) - y;
-		res = t;*/
-		res += (/*samples[k] =*/ psi(x));
+//		y = psi(x) - c;
+//		t = res + y;
+//		c = (t - res) - y;
+//		res = t;
+		res += (samples[k] = psi(x));
 	}
 	return res * dx * pi;
 }
-
+*/
 int main(int, char**) {
 #ifdef MPREC
 	mpreal::set_default_prec(prec);
 #endif
+	vex::Context ctx( vex::Filter::Type(CL_DEVICE_TYPE_GPU) && vex::Filter::DoublePrecision );
+	cout << ctx << endl;
+
 
 	string str;
 	getline(cin, str, ',');
@@ -72,9 +80,26 @@ int main(int, char**) {
 		cout << (x[k] = atoi(str.c_str())) << ',';
 	}
 	cout<<endl;
-	scalar t = 0;
-	size = 1;
-	while (size *= 10) cout<<numint()<<endl;
+
+	uint size = 1e+8;
+	double dx = double(1)/double(size - 1);
+	vex::vector<double> X(ctx, size), Y(ctx, size);
+	Y = 1;
+	vex::Reductor<double, vex::SUM> sum(ctx);
+
+	X = vex::constants::pi() * vex::element_index() * dx;
+	for (uint k = 0; k < N; k++)
+		if (N - k > 4) {
+			Y *= cos(X * x[k]) * cos(X * x[k + 1]) * cos(X * x[k + 2]);
+			k += 3;
+		} else Y *= cos(X * x[k]);
+	Y *= dx * vex::constants::pi();
+
+	cout<<sum(Y)<<endl;
+	
+//	scalar t = 0;
+//	size = 1;
+//	while (size *= 10) cout<<numint()<<endl;
 //	precalc();
 //	for (uint n = 0; n < MAX; n++) cout<<"B["<<n<<"]\t= " << Bn[n] << endl;
 //	scalar e = estimator();
